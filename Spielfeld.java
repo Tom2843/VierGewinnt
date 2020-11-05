bae656e5ba8a8369f429dfd454c286338050dae3
Jakob <ruf.jakob@gsma.schulbitum.de>
2020-11-05 08:48:52 +0100

    Spalte

--- Spielfeld.java
+++ Spielfeld.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Spielfeld
 {
     private Stein [][] Spielfeld;
-
+    
     public Spielfeld()
     {
         this.Spielfeld = new Stein [7][6];
@@ -62,6 +62,18 @@ public class Spielfeld
     private boolean pruefeVierInEinerSpalte ()
     {
         boolean erg = false;
+        for (int j = 0; j>Spielfeld.length; j++)
+        {
+            for(int i = 0; i>Spielfeld.length-4; i++)
+            {
+                if(Spielfeld[j][i].gibZeichen() == Spielfeld[j][i+1].gibZeichen() &&
+                   Spielfeld[j][i].gibZeichen() == Spielfeld[j][i+2].gibZeichen() &&
+                   Spielfeld[j][i].gibZeichen() == Spielfeld[j][i+3].gibZeichen() )
+                {
+                    return true;
+                }
+            }
+        }
         return erg;
     }       
 
